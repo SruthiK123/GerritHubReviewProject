c7eb12cb0cf3f5107d04ce34bb3c3aa6dfe9e51c
Kalavakuru <C-PRFE@Truven.com>
2019-07-05 19:07:23 +0530

    comments fixed method called

--- JavaExample.java
+++ JavaExample.java
@@ -25,5 +25,7 @@ public class JavaExample
         System.out.println("Sum of three numbers: "+obj.add(10, 20, 30));
         //This will call third add method
         System.out.println("Sum of four numbers: "+obj.add(1,  2, 3, 4));
+        //This will call mul method
+        System.out.println("Multiplication of two numbers "+obj.mul(10, 20));
     }
 }
